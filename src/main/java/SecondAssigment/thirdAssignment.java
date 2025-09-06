70f8031ac96e12153f186d3ed5a8caf88dce7338
Nourhan Khaled <khalednourhan16@gmail.com>
2025-09-06 15:47:20 +0300

    Add ThirdAssignment class to secondassignment package

---
+++ src/main/java/SecondAssigment/thirdAssignment.java
@@ -0,0 +1,193 @@
+package SecondAssigment;
+
+import java.util.Scanner;
+
+public class thirdAssignment {public static void main(String[] args) {
+ // Scanner scanner = new Scanner(System .in);
+   // System.out.println("Enter your age");
+   // int studentage = scanner .nextInt();
+   // scanner .close();
+   // System.out.println("studentage is :" +studentage);
+
+   // if(studentage>=18)
+    // System.out.println("eligable for vote");
+// else System.out.println("Not eligable for vote");
+
+
+   // Scanner scanner = new Scanner(System .in);
+   // System.out.println("Enter number to figure if even or odd");
+   // int number = scanner.nextInt();
+   // scanner.close();
+  //  if(number%2==0)
+ //   System.out.println("the number " + number +" is even");
+   // else
+   // System.out.println("the number " + number +" is odd");
+
+
+   // Scanner scanner = new Scanner(System .in);
+   // System.out.println("Enter a number to decide positive or negative");
+  //  int number = scanner.nextInt();
+ //   scanner.close();
+ //   if(number>0)
+ //   System.out.println("the number " + number + " is positive");
+ //   else if (number<0)
+  //      System.out.println("the number " + number + " is negative");
+  //  else
+   //     System.out.println("zero");
+
+
+  //  Scanner scanner = new Scanner(System .in);
+ //   System.out.println("Enter 1st number");
+ //   int firstNum= scanner.nextInt();
+ //   System.out.println("Enter 2nd number");
+ //   int secondNum= scanner.nextInt();
+ //   System.out.println("Enter 3rd number");
+ //   int thirdNum= scanner.nextInt();
+ //   if(firstNum>secondNum && firstNum>thirdNum)
+  //      System.out.println("The 1st number is the biggest");
+ //   else if (secondNum>firstNum && secondNum>thirdNum)
+ //       System.out.println("The 2nd number is the biggest");
+ //   else
+   //     System.out.println("The 3rd number is the biggest");
+
+
+   // Scanner scanner = new Scanner(System .in);
+  //  System.out.println("Enter the day number :");
+  //  int dayNumber= scanner.nextInt();
+ //   if(dayNumber==1)
+  //      System.out.println("sunday");
+//    else if(dayNumber==2)
+  //      System.out.println("monday");
+   // else if(dayNumber==3)
+     //   System.out.println("tuesday");
+   // else if(dayNumber==4)
+  //      System.out.println("wednesday");
+  //  else if(dayNumber==5)
+     //   System.out.println("thursday");
+  //  else if(dayNumber==6)
+     //  System.out.println("friday");
+  //  else if(dayNumber==7)
+      //  System.out.println("saturday");
+  //  else
+      //  System.out.println("Invalid number");
+
+   // Scanner scanner = new Scanner(System .in);
+    // System.out.println("Enter the day number :");
+    //  int dayNumber= scanner.nextInt();
+  //  switch(dayNumber)
+   // {
+   //     case 1:
+       //     System.out.println("sunday");
+      //      break ;
+      //  case 2:
+       //     System.out.println("monday");
+       //     break ;
+     //   case 3:
+        //    System.out.println("tuesday");
+     //       break ;
+     //   case 4:
+      //      System.out.println("wednesday");
+      //      break ;
+      //  case 5:
+         //   System.out.println("thursday");
+        //    break ;
+      //  case 6:
+      //      System.out.println("friday");
+       //     break ;
+      //  case 7:
+        //    System.out.println("saturday");
+        //    break ;
+      //  default:
+     //       System.out.println("Invalid number");
+   // }
+
+
+
+      //*Assignment*
+
+
+              //first question
+
+//Scanner scanner = new Scanner(System.in);
+  //  System.out.println("Enter 1st number");
+//int firstNum= scanner.nextInt();
+  //  System.out.println("Enter 2nd number");
+//int secondNum= scanner.nextInt();
+//if (firstNum>secondNum)
+   // System.out.println("The 1st number is bigger");
+//else
+   // System.out.println("The 2nd number is bigger");
+
+
+                  // second question
+
+
+ //   Scanner scanner = new Scanner(System.in);
+  //   System.out.println("Enter 1st number");
+  //   int firstNum= scanner.nextInt();
+  //  System.out.println("Enter 2nd number");
+ //   int secondNum= scanner.nextInt();
+//    System.out.println("Enter 3rd number");
+ //   int thirdNum= scanner.nextInt();
+// if (firstNum<secondNum && firstNum<thirdNum)
+ //   System.out.println("The 1st number is the smallest");
+//  else if (secondNum<firstNum && secondNum<thirdNum)
+ //   System.out.println("The 2nd number is the smallest");
+//  else
+  //  System.out.println("The 3rd number is the smallest");
+
+
+                  // Third question
+
+
+    Scanner scanner = new Scanner(System.in);
+     System.out.println("Enter Day name");
+    String dayName = scanner.next();
+
+    switch (dayName)
+    {
+        case "sunday" :
+            System.out.println("The number of day is 1");
+        break;
+        case "monday" :
+            System.out.println("The number of day is 2");
+            break;
+        case "tuesday" :
+            System.out.println("The number of day is 3");
+            break;
+        case "wednesday" :
+            System.out.println("The number of day is 4");
+            break;
+        case "thursday" :
+            System.out.println("The number of day is 5");
+            break;
+        case "friday" :
+            System.out.println("The number of day is 6");
+            break;
+        case "saturday" :
+            System.out.println("The number of day is 7");
+            break;
+        default:
+            System.out.println("Invalid Day Name");
+
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+}
+}
